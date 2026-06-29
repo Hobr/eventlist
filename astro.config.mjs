10d3b0f7f6fb5dd0bcf09d51462fcd244a106138
Hobr <mail@hobr.site>
2026-06-29 15:23:36 +0800

    format: run

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [svelte()]
+    adapter: cloudflare(),
+    integrations: [svelte()],
 });
