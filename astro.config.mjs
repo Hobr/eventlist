1c1aaee983f165cfe9c0d80cca716f5b9ccfb087
Hobr <mail@hobr.site>
2026-07-01 13:18:46 +0800

    lucide: add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
-
 import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     adapter: cloudflare(),
-    integrations: [svelte()]
+    integrations: [svelte()],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
