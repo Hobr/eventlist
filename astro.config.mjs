a173a6878047d9be0068befe15371e8dc0d4bfad
Hobr <mail@hobr.site>
2026-07-31 13:05:54 +0800

    sitemap: add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,26 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://acg.hobr.site",
     adapter: cloudflare(),
-    integrations: [svelte()],
-
+    integrations: [
+        svelte(),
+        sitemap({
+            filter: (page) => {
+                const pathname = new URL(page).pathname;
+                return !(
+                    pathname === "/admin" ||
+                    pathname.startsWith("/admin/") ||
+                    pathname === "/api" ||
+                    pathname.startsWith("/api/")
+                );
+            }
+        })
+    ],
     vite: {
         plugins: [tailwindcss()]
     }
