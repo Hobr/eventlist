39571388d3b4966dd2df56a3d7d577e4de523f6a
Hobr <mail@hobr.site>
2026-07-31 16:34:30 +0800

    option: edit EVENT_TYPES & EVENT_SCALES

--- src/lib/events/options.ts
+++ src/lib/events/options.ts
@@ -4,21 +4,25 @@ export interface EventOption {
 }
 
 export const EVENT_TYPES = [
-    { name: "comic", label: "漫展" },
+    { name: "comic", label: "综合商业展" },
     { name: "doujin", label: "同人展" },
-    { name: "concert", label: "演唱会" },
-    { name: "stage", label: "舞台剧·2.5次元" },
-    { name: "dance", label: "舞见·宅舞" },
-    { name: "ipflash", label: "IP主题快闪" },
+    { name: "concert", label: "演唱会/演奏会" },
+    { name: "only", label: "Only展" },
+    { name: "meeting", label: "见面会" },
+    { name: "stage", label: "舞台剧" },
+    { name: "dance", label: "宅舞" },
+    { name: "ipflash", label: "IP快闪/联动" },
+    { name: "exhibition", label: "展览" },
     { name: "online", label: "线上活动" },
     { name: "other", label: "其它" }
 ] as const satisfies readonly EventOption[];
 
 export const EVENT_SCALES = [
-    { name: "small", label: "小型(地区级)" },
-    { name: "mid", label: "中型(省级)" },
-    { name: "large", label: "大型(全国级)" },
-    { name: "mega", label: "超大型(国际级)" }
+    { name: "mini", label: "微型 (100人以下)" },
+    { name: "small", label: "小型 (100 - 1,000人)" },
+    { name: "mid", label: "中型 (1,000 - 5,000人)" },
+    { name: "large", label: "大型 (5,000 - 10,000人)" },
+    { name: "mega", label: "超大型 (1万人以上)" }
 ] as const satisfies readonly EventOption[];
 
 export type EventType = (typeof EVENT_TYPES)[number]["name"];
